63d2392473f5594c59a974d4b61271d09b4b473a
YOSHIKAWA,Shiina <219c0446@s.obirin.ac.jp>
2020-07-09 18:39:12 +0900

    スタイル変えた

--- public/main.js
+++ public/main.js
@@ -3,13 +3,30 @@ const form = document.getElementById("form");
 const input = document.getElementById("msg");
 const chats = document.getElementById("chats");
 
+const nameform = document.getElementById("nameform");
+const name = document.getElementById("name");
+
+let username='';
+nameform.addEventListener('submit', function(event){
+  username = name.value;
+  event.preventDefault();
+  nameform.style.display ="none";
+  form.style.display ="block";
+})
+
 form.addEventListener('submit', function(event){
-  socketio.emit('message', input.value);
+  const msg = JSON.stringify({msg: input.value, name: username})
+  socketio.emit('message', msg);
   input.value='';
   event.preventDefault();
 })
 socketio.on('message',function(msg){
-  const li = document.createElement("li");
-  li.append(msg);
-  chats.append(li);
+  const obj = JSON.parse(msg);
+
+  const dt = document.createElement("dt");
+  const dd = document.createElement("dd");
+  dt.append(obj.name);
+  chats.append(dt);
+  dd.append(obj.msg);
+  chats.append(dd);
 });
